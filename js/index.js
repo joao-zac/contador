53dbb7f0f1eaae596caa7296eae67954cb2e9f76
Jose Zacarias <joao01081337@gmail.com@git config --global user.name joao-zacgit config --global user.email joao01081337@gmail.com>
2022-04-29 17:23:01 -0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,32 +1,75 @@
-const btn = document.querySelector("#enviar");
 
-function mascara(i){
-   
-    var v = i.value;
-    
-    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
-       i.value = v.substring(0, v.length-1);
-       return;
-    }
-    
-    i.setAttribute("maxlength", "10");
-    if (v.length == 2 || v.length == 5) i.value += "/";
+var date;
+
+window.onload = () => {
+	contador()
 }
 
+function formatDate(date) {
+	date = date.split("/");
+	date = date.reverse().toString().replaceAll(",", "-");
 
+	return date;
+}
 
-btn.addEventListener("click", function(e) {
+function reiniciar() {
+	window.location.href = "index.html"
+}
 
-    e.preventDefault();
+function getDate() {
+	date = document.getElementById("date").value;
+	sessionStorage.setItem("data", date);
+	checagem();
+}
 
-    const numero = document.querySelector("#contador_input");
-    const value = numero.value;
-    
-    console.log(value);
-});
+function checagem() {
+	date = sessionStorage.getItem("data");
+	date = formatDate(date);
 
+	let dataFormatada = new Date(date + ":00:00:00").getTime();
+	let now = new Date().getTime();
 
+	if (dataFormatada - now < 0 || date == "") {
+		alert("Por favor, insira uma data maior que a atual!");
+	} else {
+		window.location.href = "./contador.html";
+		contador();
+	}
+}
 
+function contador() {
+	date = sessionStorage.getItem("data");
+	date = formatDate(date);
+	countDown = new Date(date + ":00:00:00");
 
+	console.log(countDown);
 
+	countDown.getTime();
 
+	console.log(countDown.getTime());
+
+	var x = setInterval(function () {
+		var now = new Date().getTime();
+
+		var distance = countDown - now;
+
+		var dias = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var horas = Math.floor(
+			(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		var minutos = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var segundos = Math.floor((distance % (1000 * 60)) / 1000);
+
+		console.log(dias, horas, minutos, segundos);
+
+		document.getElementById("dias").innerHTML = dias;
+		document.getElementById("horas").innerHTML = horas;
+		document.getElementById("minutos").innerHTML = minutos;
+		document.getElementById("segundos").innerHTML = segundos;
+
+		if (distance < 0) {
+			clearInterval(x);
+			document.getElementById("demo").innerHTML = "EXPIRED";
+		}
+	}, 1000);
+}
